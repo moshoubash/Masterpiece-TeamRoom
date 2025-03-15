a86784ad064799c2c9593c903da4045f548ca48e
Mohammad Shoubash <mohammedshobash2002@gmail.com>
2025-03-15 07:57:26 +0300

    add dashboard

---
+++ FrontEnd/dashboard/components/sidebar.php
@@ -0,0 +1,73 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+			<div class="sidebar-content js-simplebar">
+				<a class="sidebar-brand" href="/p2p/FrontEnd/dashboard/">
+					<span class="align-middle">Admin Dashboard</span>
+				</a>
+
+				<ul class="sidebar-nav">
+					<li class="sidebar-header">
+						Pages
+					</li>
+
+					<li class="sidebar-item active">
+						<a class="sidebar-link" href="/p2p/FrontEnd/dashboard">
+							<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Home</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/users.php">
+							<i class="align-middle" data-feather="user"></i> <span class="align-middle">Users</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/listings.php">
+							<i class="align-middle" data-feather="list"></i> <span class="align-middle">Listings</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/bookings.php">
+							<i class="align-middle" data-feather="menu"></i> <span class="align-middle">Bookings</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/reviews.php">
+							<i class="align-middle" data-feather="star"></i> <span class="align-middle">Reviews</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/messages.php">
+							<i class="align-middle" data-feather="mail"></i> <span class="align-middle">Messages</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/payments.php">
+							<i class="align-middle" data-feather="credit-card"></i> <span class="align-middle">Payments</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/reports.php">
+							<i class="align-middle" data-feather="file"></i> <span class="align-middle">Reports</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="/p2p/frontend/dashboard/pages/settings.php">
+							<i class="align-middle" data-feather="settings"></i> <span class="align-middle">Settings</span>
+						</a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="#">
+							<i class="align-middle" data-feather="log-out"></i> <span class="align-middle">Logout</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
